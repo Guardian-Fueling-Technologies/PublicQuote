4d31f2569cbee08636ecb0ab33ad570977db3b7b
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-10-11 16:34:13 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -200,8 +200,9 @@ def techPage():
             # </table>
             # """
 
-            st.write(f"**Price (Pre-Tax):**  ${total_price:.2f},       **Estimated Sales Tax:**  ${total_price*taxRate/100:.2f},       **Total (including tax):** ${total_price_with_tax:.2f}")
-            st.write(f"**Price (Pre-Tax):**  ${total_price:.2f},       **Estimated Sales Tax:**  ${total_price*taxRate/100:.2f},       **Total (including tax):**  ${total_price_with_tax:2.f}")
+            printH = f"**Price (Pre-Tax):  ${total_price:.2f},       Estimated Sales Tax:  ${total_price*taxRate/100:.2f},       Total (including tax):  ${total_price_with_tax:.2f}"
+            st.write(printH)
+            st.write(f"**Price (Pre-Tax):**  ${total_price:.2f},       **Estimated Sales Tax:**  ${total_price*taxRate/100:.2f},       **Total (including tax):**  ${total_price_with_tax:.2f}")
             
             input_pdf = PdfReader(open('input.pdf', 'rb'))
             buffer = io.BytesIO()
